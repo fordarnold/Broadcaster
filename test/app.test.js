1ee66034cc3e368e953d724f017d0c9e04ebaf19
Robin Hood <fordarnold@gmail.com>
2019-11-27 23:55:21 +0200

    Complete first test successfully :)
    I think I'm ready to start working on real project tests now.
    
    ~https://dev.to/easybuoy/testing-node-api-with-mocha-chai-248b
    
    [#169994359]

--- test/app.test.js
+++ test/app.test.js
@@ -8,7 +8,28 @@ chai.use(chaiHttp);
 
 describe("Server!", () => {
 
-	it("welcomes user to the api", done => {});
+	it("welcomes user to the api", done => {
+		chai
+			.request(app)
+			.get("/")
+			.end((err, res) => {
+				expect(res).to.have.status(200);
+				expect(res.body.status).to.equals("success");
+				expect(res.body.message).to.equals("Welcome To Testing API");
+				done();
+			});
+	});
 
-	it("adds 2 numbers", done => {});
+	it("adds 2 numbers", done => {
+		chai
+			.request(app)
+			.post("/add")
+			.send({ num1: 5, num2: 5 })
+			.end((err, res) => {
+				expect(res).to.have.status(200);
+				expect(res.body.status).to.equals("success");
+				expect(res.body.result).to.equals(10);
+				done();
+			});
+	});
 });
