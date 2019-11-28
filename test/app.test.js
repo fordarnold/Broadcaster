08aea16fead630f2f25273c57b64124e8a0ceded
Robin Hood <fordarnold@gmail.com>
2019-11-28 22:47:08 +0200

    Write unit tests for common API endpoints found in app.js
    [#169994359]

--- test/app.test.js
+++ test/app.test.js
@@ -1,35 +1,50 @@
-const app = require("../server/app");
-const chai = require("chai");
-const chaiHttp = require("chai-http");
+import chai from "chai";
+import chaiHttp from "chai-http";
+import app from "../server/app";
 
 const { expect } = chai;
 
 chai.use(chaiHttp);
 
-describe("Server!", () => {
+/**
+ * TEST SUITES AND TEST CASES
+ *
+ * ~https://www.robinwieruch.de/node-js-testing-mocha-chai
+ * -----------------------------------------------------------------
+ *
+ * 	describe('test suite', () => {
+ * 		it('test case', () => {	
+ *			...
+ *		});
+ *	});
+ *
+ * -----------------------------------------------------------------
+ */
 
-	it("welcomes user to the api", done => {
-		chai
-			.request(app)
+describe("API Server - Starter Tests", () => {
+
+	it("should welcome the user to the API server", done => {
+
+		chai.request(app)
 			.get("/")
 			.end((err, res) => {
 				expect(res).to.have.status(200);
-				expect(res.body.status).to.equals("success");
-				expect(res.body.message).to.equals("Welcome To Testing API");
+				expect(res.body.status).to.equals(200);
+				expect(res.body.message).to.equals("Broadcaster API Server welcomes you!");
 				done();
 			});
 	});
 
-	it("adds 2 numbers", done => {
-		chai
-			.request(app)
-			.post("/add")
-			.send({ num1: 5, num2: 5 })
-			.end((err, res) => {
-				expect(res).to.have.status(200);
-				expect(res.body.status).to.equals("success");
-				expect(res.body.result).to.equals(10);
-				done();
-			});
-	});
+	it("should return an error response for missing urls", done => {
+
+        chai.request(app)
+            .get("/missing")
+            .end((err, res) => {
+            	expect(res).to.have.status(404);
+            	expect(res.body.status).to.equals(404);
+            	expect(res.body.error).to.equals("API endpoint was not found");
+                done();
+            });
+    });
+
 });
