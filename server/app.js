bae1d02aa236857953bd68e486040d5e06536416
Robin Hood <fordarnold@gmail.com>
2019-11-28 16:43:10 +0200

    Enable ES6 features (like 'import' statements) using Babel
    Usually, node-based apps use 'require' to import modules. With Babel,
    the code is transpiled to enable latest features of JavaScript
    
    ~https://www.freecodecamp.org/news/how-to-enable-es6-and-beyond-syntax-with-node-and-express-68d3e11fe1ab/
    
    [#170016027]

--- server/app.js
+++ server/app.js
@@ -1,25 +1,28 @@
-// import the Express framework
-const express = require("express");
-// // import joi, data validation library for JS
-// const joi = require('joi');
-// // import app routes
-// import v1Routes from './v1/routes/appRoutes';
+import express from 'express'; // Express web framework
+import bodyParser from 'body-parser'; // Handle HTTP POST requests with 'req.body' in Express framework (~https://stackoverflow.com/questions/38306569/what-does-body-parser-do-with-express)
+// const joi = require('joi'); // Data validation library for JS
 
-// create the Express app server
-const app = express();
-// specify the server port
-const PORT = process.env.PORT || 3000;
+import routes from './routes/app-routes'; // Import API routes
 
-// starting (landing) endpoint
-// app.get('/', (req, res) => {
-//     res.send('Welcome to Broadcaster API Server');
-// });
+const app = express(); // Create the Express app server
+const PORT = process.env.PORT || 3000; // Specify the server port
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(routes);
+app.use('/uploads', express.static('uploads'));
+
+// default (landing) endpoint
 app.get('/', (req, res) => {
+
     res.status(200).json({ 
     	status: 200,
     	message: 'Broadcaster API Server welcomes you!'
     });
+    
 });
 
 // start the server, on specified port
 app.listen(PORT, () => console.log(`Broadcaster API Server is listening on port ${PORT} ...`));
+
+export default app;
